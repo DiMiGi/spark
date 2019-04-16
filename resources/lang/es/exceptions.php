7bc2a1f9d646c2c1c9675db42d541a3fc91ed3bb
Diego Miranda Gipoulou <diego.miranda.g@usach.cl>
2019-04-16 15:39:47 -0400

    Spanish language checkout, will be added latter from another branch

--- resources/lang/es/exceptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'TaskDoesNotHaveUsersException' => 'La tarea ":task" no está asignada a un usuario o grupo. Debe seleccionar un usuario o un grupo.',
-    'ScriptLanguageNotSupported' => 'El uso del lenguaje ":language" no se encuentra implementado',
-    'SyntaxErrorException' => 'Error de sintaxis. :error',
-    'ExpressionFailedException' => 'Error al evaluar la expresión. :error',
-];
